94b5a8f8d89c1a206597a7be2e87087bd06e533e
anujnaRao <anujna.p.rao@gmail.com>
2020-07-06 09:40:46 +0530

    Props and State

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
-
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-    {/* <Greet/>
-    <Welcome /> */}
-    <Hello />
+
+    <Message />
+    {/* <Greet name="Anujna" age="22">
+      <p>This is to have props unknown</p>
+    </Greet>
+    <Greet name="Anjali" age="26">
+      <button>Save</button>
+    </Greet>
+    <Welcome name="Mangala" age="31"/>
+    <Welcome name="Poornima" age="26"/> */}
+    {/* <Hello /> */}
     </div>
   );
 }
