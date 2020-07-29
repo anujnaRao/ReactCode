527aaa313ea33c15ebd8c3d9b05e1d249e7db784
anujnaRao <anujna.prao5974@yahoo.com>
2020-07-29 10:31:16 +0530

    Event Handling

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -5,26 +5,31 @@ import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
 
-    {/* <Counter/> */}
+    <ClassClick/>
+    {/* <FunctionClick/> */}
 
-     {/* <Message/>
+    {/* <Counter/>
+
+     <Message/>
     <Greet name="Anujna" age="22">
       <p>This is to have props unknown</p>
     </Greet>
     <Greet name="Anjali" age="26">
       <button>Save</button>
-    </Greet> */}
+    </Greet>
     <Greet name="Muskan" age="22"/>
 
     <Welcome name="Mangala" age="31"/>
 
-    {/* <Welcome name="Poornima" age="26"/>
-    <Hello />  */}
+    <Welcome name="Poornima" age="26"/> */}
+    <Hello /> 
     </div>
   );
 }
